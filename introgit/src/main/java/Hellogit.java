4ab5da5506692d50e74eefcda37bc33c42f8d623
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-15 10:28:54 +0200

    Changed one more time

--- introgit/src/main/java/Hellogit.java
+++ introgit/src/main/java/Hellogit.java
@@ -1,6 +1,6 @@
 public class Hellogit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Idea and Git");
+        System.out.println("Hello Idea and Git and Java");
     }
 }
