833a6bba00d194a025cd175fc9bb4d94b63c97f5
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-16 16:53:16 +0200

    finished Registration project

--- training-solutions/src/main/java/classstructureio/Registration.java
+++ training-solutions/src/main/java/classstructureio/Registration.java
@@ -1,4 +1,19 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg a nevét:");
+        String name = scanner.nextLine();
+
+        System.out.println("Adja meg az e-mail cimét:");
+        String emailAddress = scanner.nextLine();
+
+        System.out.println("A regisztracios adatok:");
+        System.out.println("Név: " + name);
+        System.out.println("E-mail cim: " + emailAddress);
+    }
 }
