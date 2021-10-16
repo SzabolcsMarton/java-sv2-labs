f1d3131886e4f2f2301c99e978ac9297020d2c6c
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-16 16:34:30 +0200

    finished classstructureio/calculator project

--- training-solutions/src/main/java/classstructureio/Calculator.java
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -1,7 +1,17 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Irj be egy tetszőleges számot :");
+        int number1 = scanner.nextInt();
+        System.out.println("Irj be még egy számot :");
+        int number2 = scanner.nextInt();
+        System.out.println(number1 + "+" + number2);
+        System.out.println("Összesen :");
+        System.out.println( number1 + number2);
     }
 }
