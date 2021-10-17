5d7f54261999e4eedeb900b2f7a3a47504a3b96c
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-17 20:47:58 +0200

    add attributes and methods

--- training-solutions/src/main/java/classstructuremethods/Client.java
+++ training-solutions/src/main/java/classstructuremethods/Client.java
@@ -1,5 +1,36 @@
 package classstructuremethods;
 
 public class Client {
+    private String name;
+    private int yearOfBirth;
+    private String address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public  void migrate(String newAddress) {
+        address = newAddress;
+    }
 }
