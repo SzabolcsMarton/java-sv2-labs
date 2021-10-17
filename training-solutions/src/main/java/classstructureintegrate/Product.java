8256edc7f31aac0d16082719adc74be4e211192c
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-18 00:09:38 +0200

    add all methods and attrbs. in Product class, created ProductMain class

--- training-solutions/src/main/java/classstructureintegrate/Product.java
+++ training-solutions/src/main/java/classstructureintegrate/Product.java
@@ -1,4 +1,27 @@
 package classstructureintegrate;
 
 public class Product {
+    private  String name;
+    private  int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void increasePrice(int increaseAmountmount) {
+        price += increaseAmountmount;
+    }
+
+    public void decreasePrice(int decreaseAmount) {
+        price -= decreaseAmount;
+    }
 }
