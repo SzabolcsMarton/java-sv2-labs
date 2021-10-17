10772cb3a871cbfc624605843439ae1da8dd91e8
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-18 00:24:43 +0200

    add all methods and attrbs. in ProductMain class, finished project

--- training-solutions/src/main/java/classstructureintegrate/ProductMain.java
+++ training-solutions/src/main/java/classstructureintegrate/ProductMain.java
@@ -1,4 +1,27 @@
 package classstructureintegrate;
 
+import java.util.Scanner;
+
 public class ProductMain {
+
+    public static void main(String[] args) {
+
+        Scanner productScanner = new Scanner(System.in);
+        System.out.println("Name of Product:");
+        String nameOfProduct = productScanner.nextLine();
+
+        System.out.println("Price: ");
+        int priceOfProduct = productScanner.nextInt();
+
+        Product product = new Product(nameOfProduct,priceOfProduct);
+        System.out.println("Name: " + product.getName() + "  Price: " + product.getPrice());
+
+        product.increasePrice(200);
+        System.out.println("Name: " + product.getName() + "  Price: " + product.getPrice());
+
+        product.decreasePrice(100);
+        System.out.println("Name: " + product.getName() + "  Price: " + product.getPrice());
+
+    }
 }
+
