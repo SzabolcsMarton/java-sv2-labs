49ce47a6ef751a10965b071de91ea18b49dc2ef4
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-19 10:28:27 +0200

    add variables and a block , projectfinished

--- training-solutions/src/main/java/localvariables/LocalVariablesMain.java
+++ training-solutions/src/main/java/localvariables/LocalVariablesMain.java
@@ -1,4 +1,39 @@
 package localvariables;
 
 public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+        boolean b;
+        //System.out.println(b); // not possible
+        b = false;
+        System.out.println(b);
+
+        int a = 2;
+        System.out.println(a);
+
+        int i = 3;
+        int j = 4;
+        int k = i;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+
+        //System.out.println(f);  not possible as well.
+
+        String s = "Hello World!";
+        System.out.println(s);
+        String t = s;
+        System.out.println(t);
+
+        {
+            int x = 0;
+            System.out.println(x);
+            System.out.println(a);
+
+        }
+
+        //System.out.println(x); not working
+
+
+    }
 }
