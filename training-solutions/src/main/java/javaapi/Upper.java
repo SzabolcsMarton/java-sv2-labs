2f5db8ef44cc9e38826c1d7714af752b2967517f
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-21 00:19:38 +0200

    create javaapi package and Upper class

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,8 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        System.out.println(text.toUpperCase());
+    }
+}
