fcc0ea11093a8dd8fbbcebafe6ea8e642066975d
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-20 23:31:52 +0200

    add package and class to packages. prefix, firstName, lastName

---
+++ training-solutions/src/main/java/packages/lastName/lastName.java
@@ -0,0 +1,13 @@
+package packages.lastName;
+
+public class lastName {
+    private String lastName;
+
+    public lastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ training-solutions/src/main/java/packages/prefix/Prefix.java
@@ -0,0 +1,13 @@
+package packages.prefix;
+
+public class Prefix {
+    private  String pre;
+
+    public Prefix(String prefix) {
+        pre = prefix;
+    }
+
+    public String getPre() {
+        return pre;
+    }
+}
