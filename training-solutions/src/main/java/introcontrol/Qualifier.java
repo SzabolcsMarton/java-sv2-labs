4bbb4af2da486290f580365fb07d0fdef66bf6bc
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-21 23:56:25 +0200

    add main to class Qualifier

--- training-solutions/src/main/java/introcontrol/Qualifier.java
+++ training-solutions/src/main/java/introcontrol/Qualifier.java
@@ -1,4 +1,16 @@
 package introcontrol;
 
+import java.util.Scanner;
+
 public class Qualifier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in a number!");
+        int number = scanner.nextInt();
+         if( number > 100) {
+             System.out.println("Nagyobb, mint szaz!");
+         }else {
+             System.out.println("Szaz vagy kissebb");
+         }
+    }
 }
