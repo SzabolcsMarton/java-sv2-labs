610d51c9583f0ff6c91931a75d22223b6dd135e4
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-21 17:24:17 +0200

    update StringTypeMain class

--- training-solutions/src/main/java/stringtype/StringTypeMain.java
+++ training-solutions/src/main/java/stringtype/StringTypeMain.java
@@ -23,7 +23,8 @@ public class StringTypeMain {
 
         String abcde = "Abcde";
         System.out.println(abcde.length());
-        System.out.println(abcde.substring(0,1) + "," + abcde.substring(2,3));
+        System.out.println(abcde.substring(0,1) + "," + abcde.substring(2,3)); //version 1
+        System.out.println(abcde.charAt(0) + "," + abcde.charAt(2)); //version 2
         System.out.println(abcde.substring(0,3));
 
     }
