2ddfc6c06742c648f04a072029c2b8d2d59e5da0
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-21 13:46:40 +0200

    add stringtypr.StringTypeMain with full main method

---
+++ training-solutions/src/main/java/stringtype/StringTypeMain.java
@@ -0,0 +1,30 @@
+package stringtype;
+
+public class StringTypeMain {
+    public static void main(String[] args) {
+        String prefix ="Hello ";
+        String name = "John Doe";
+        String message = prefix + name;
+        message += 444;
+        boolean b = message.equals("Hello John Doe");
+        boolean c = message.equals("Hello John Doe444");
+
+        System.out.println(prefix);
+        System.out.println(name);
+        System.out.println(message);
+        System.out.println(b);
+        System.out.println(c);
+
+        String empty1 = "";
+        String empty2 = "";
+        String empty3 = empty1 + empty2;
+        System.out.println(empty3);
+        System.out.println(empty3.length());
+
+        String abcde = "Abcde";
+        System.out.println(abcde.length());
+        System.out.println(abcde.substring(0,1) + "," + abcde.substring(2,3));
+        System.out.println(abcde.substring(0,3));
+
+    }
+}
