8a5e6b537124655f52fcabfb8f77a397e1926704
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-22 12:06:00 +0200

    add daysOfTheWeek , powOfTwoo

--- training-solutions/src/main/java/array/ArrayMain.java
+++ training-solutions/src/main/java/array/ArrayMain.java
@@ -1,4 +1,22 @@
 package array;
 
 public class ArrayMain {
+
+    public static void main(String[] args) {
+//        String [] daysOfTheWeek = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+//        System.out.println(daysOfTheWeek[1]);
+//        System.out.println(daysOfTheWeek.length);
+
+        int [] powOfTwoo = new int[5];
+            powOfTwoo [0] = 1;
+            for(int i = 1; i < powOfTwoo.length; i++) {
+                powOfTwoo[i] = powOfTwoo[ i - 1 ] * 2;
+            }
+
+        for (int j: powOfTwoo){
+            System.out.print(j + ", ");
+        }
+    }
+
+
 }
