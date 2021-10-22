497c5ffc62a6c090bd40cf79c0144c71b7962327
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-22 14:20:10 +0200

    add cmdarguments.flowers main()

---
+++ training-solutions/src/main/java/cmdarguments/flower.java
@@ -0,0 +1,10 @@
+package cmdarguments;
+
+public class flower {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for ( int i = 0; i < args.length; i++){
+            System.out.println((i + 1) + ". elem: " + args[i]);
+        }
+    }
+}
