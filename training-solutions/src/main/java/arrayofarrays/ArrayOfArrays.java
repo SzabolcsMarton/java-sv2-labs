cd1f82229f13183b4907e3d31087edb9424bacf5
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-22 20:25:23 +0200

    created arraofarrays.ArraysOfArrays add printArrayOfArrays and main method

---
+++ training-solutions/src/main/java/arrayofarrays/ArrayOfArrays.java
@@ -0,0 +1,20 @@
+package arrayofarrays;
+
+public class ArrayOfArrays {
+
+    public void printArrayOfArrays(int[][] a) {
+        for(int i[]: a){
+           for( int j: i){
+               System.out.print(j + " ");
+           }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayOfArrays arrayOfArrays = new ArrayOfArrays();
+        int[][] array = {{0, 1, 2}, {0, 1, 2}, {0, 1, 2}, {0, 1, 2}};
+
+        arrayOfArrays.printArrayOfArrays(array);
+    }
+}
