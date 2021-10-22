3eff7dd1a5f24abf421d9ceba58a71e8ff817840
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-22 12:18:53 +0200

    add sixBooleans array and loops

--- training-solutions/src/main/java/array/ArrayMain.java
+++ training-solutions/src/main/java/array/ArrayMain.java
@@ -7,16 +7,23 @@ public class ArrayMain {
 //        System.out.println(daysOfTheWeek[1]);
 //        System.out.println(daysOfTheWeek.length);
 
-        int [] powOfTwoo = new int[5];
-            powOfTwoo [0] = 1;
-            for(int i = 1; i < powOfTwoo.length; i++) {
-                powOfTwoo[i] = powOfTwoo[ i - 1 ] * 2;
-            }
+//        int[] powOfTwoo = new int[5];
+//        powOfTwoo[0] = 1;
+//        for (int i = 1; i < powOfTwoo.length; i++) {
+//            powOfTwoo[i] = powOfTwoo[i - 1] * 2;
+//        }
+//
+//        for (int j : powOfTwoo) {
+//            System.out.print(j + ", ");
+//        }
 
-        for (int j: powOfTwoo){
+        boolean[] sixBooleans = new boolean[6];
+        sixBooleans[0] = false;
+        for (int i =1; i < sixBooleans.length; i++) {
+            sixBooleans[i] = !sixBooleans[ i - 1 ];
+        }
+        for ( boolean j: sixBooleans) {
             System.out.print(j + ", ");
         }
     }
-
-
 }
