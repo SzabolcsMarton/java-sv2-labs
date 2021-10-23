1b22591ad64ae491b1b7671788ad4eef055a38e7
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-23 16:05:24 +0200

    add main() and tested all the methods

--- training-solutions/src/main/java/arrays/ArraysMain.java
+++ training-solutions/src/main/java/arrays/ArraysMain.java
@@ -3,10 +3,10 @@ package arrays;
 import java.util.Arrays;
 
 public class ArraysMain {
-//    public  String numberOfDaysAsString() {
-//        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-//        return Arrays.toString(numberOfDays);
-//    }
+    public  String numberOfDaysAsString() {
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
 
     public  String multiplicationTableAsString(int size) {
         int[][] multiplicTable = new int[size][size];
@@ -34,18 +34,48 @@ public class ArraysMain {
     }
 
     public boolean wonLottery(int[] fortune, int[] player) {
-        Arrays.sort(fortune);
-        Arrays.sort(player);
-        return Arrays.equals(fortune, player);
+//        Arrays.sort(fortune);
+//        Arrays.sort(player);
+//        return Arrays.equals(fortune, player);
+
+          int[] arrayFortune = Arrays.copyOfRange(fortune, 0, fortune.length);
+          int[] arrayPlayers = Arrays.copyOfRange(player, 0, player.length);
+          Arrays.sort(arrayFortune);
+          Arrays.sort(arrayPlayers);
+          return  Arrays.equals(arrayFortune,arrayPlayers);
+
+
+
     }
 
-
-
-
-
-
     public static void main(String[] args) {
+        double[] temps1 = {10.2, 10.8, 14.5, 15.6, 16.0, 17.5, 18.5, 19.0, 20.2, 22.0, 21.4, 20.8, 21.3, 20.1, 19.5, 18.4, 17.6, 16.5, 15.4, 15.0, 14.7, 13.5, 12.4, 11.8};
+        double[] temps2 = {10.2, 10.8, 12.5, 15.3, 15.0, 17.1, 18.5, 19.0, 20.2, 21.0, 20.4, 20.9, 21.0, 20.1, 18.5, 18.4, 16.1, 16.5, 16.4, 15.0, 14.7, 13.5, 12.4, 11.8};
+        double[] controlArrayTemp1 = {10.2, 10.8, 14.5, 15.6, 16.0, 17.5, 18.5, 19.0, 20.2, 22.0, 21.4, 20.8, 21.3, 20.1, 19.5, 18.4, 17.6, 16.5, 15.4, 15.0, 14.7, 13.5, 12.4, 11.8};
+        double[] temp23fortest = {10.2, 10.8, 14.5, 15.6, 16.0, 17.5, 18.5, 19.0, 20.2, 22.0, 21.4, 20.8, 21.3, 20.1, 19.5, 18.4, 17.6, 16.5, 15.4, 15.0, 14.7, 13.5, 12.4};
+        int[] winnerArray = {11, 43, 5, 56, 32};
+        int[] playerArray = {15, 56, 78, 64, 3};
+
+        System.out.println("winnerArray: " + Arrays.toString(winnerArray));
+        System.out.println("playerArray: " + Arrays.toString(playerArray));
+
         ArraysMain arraysMain = new ArraysMain();
-        System.out.println(arraysMain.multiplicationTableAsString(5));
+
+        System.out.println(arraysMain.numberOfDaysAsString());
+        System.out.println();
+        System.out.println(arraysMain.multiplicationTableAsString(6));
+        System.out.println();
+        System.out.println(arraysMain.sameTempValues(temps1,temps2));
+        System.out.println(arraysMain.sameTempValues(temps1,controlArrayTemp1));
+        System.out.println();
+        System.out.println(arraysMain.sameTempValuesDaylight(temps1, temps2));
+        System.out.println(arraysMain.sameTempValuesDaylight(temps1,temp23fortest));
+        System.out.println();
+        System.out.println(arraysMain.wonLottery(winnerArray,playerArray));
+        System.out.println();
+        System.out.println(Arrays.toString(winnerArray));
+        System.out.println(Arrays.toString(playerArray));
+
+
     }
 }
