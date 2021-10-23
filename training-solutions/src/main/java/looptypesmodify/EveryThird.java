4a412339ceb4abe5c4983573b90e4ad30ee0657b
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-24 00:08:36 +0200

    created looptypesmodify.EveryThird add changeToZero method and tested in main()

---
+++ training-solutions/src/main/java/looptypesmodify/EveryThird.java
@@ -0,0 +1,19 @@
+package looptypesmodify;
+
+import java.util.Arrays;
+
+public class EveryThird {
+    public int[] changeToZero(int[]numbers) {
+        for(int i = numbers.length - 1; i > 0; i -= 3) {
+            numbers[i] =0;
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        int[] originalArray = {2, 6, 3, 5, 7, 2, 6, 2, 3, 5, 7, 3, 2, 9};
+        EveryThird everyThird = new EveryThird();
+        int[] changedArray = everyThird.changeToZero(originalArray);
+        System.out.println(Arrays.toString(changedArray));
+    }
+}
