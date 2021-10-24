63bc642c05a34b11bc5cc9d7b62f643e322e29fc
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-24 19:09:06 +0200

    add math.math.MathMethods class

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,24 @@
+package math.math;
+
+import java.util.Random;
+
+public class MathMethods {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,8));
+        System.out.println(Math.min(5,8));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(15.54));
+        System.out.println(Math.abs(-8));
+        System.out.println(Math.negateExact(-5));
+        System.out.println(Math.addExact(5,6));
+        System.out.println(Math.subtractExact(10,5));
+        System.out.println(Math.multiplyExact(5, 5));
+        System.out.println(Math.pow(5.5, 5.5));
+        System.out.println(Math.incrementExact(10));
+        System.out.println(Math.decrementExact(10));
+        System.out.println(Math.random());
+
+    }
+
+}
