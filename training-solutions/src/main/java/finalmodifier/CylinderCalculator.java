7f671bc83b7bdd5550fde3fa6128b6220cc49ecc
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-24 16:57:12 +0200

    add CircleCalculator and CylinderCalculator classes

---
+++ training-solutions/src/main/java/finalmodifier/CylinderCalculator.java
@@ -0,0 +1,10 @@
+package finalmodifier;
+
+public class CylinderCalculator {
+    public double calculateVolume(double r, double h) {
+        return r * r * h * CircleCalculator.PI;
+    }
+    public double CalculateSurfaceArea( double r, double h) {
+        return ( 2 * ( r * r) * CircleCalculator.PI ) + (2 * r * CircleCalculator.PI * h);
+    }
+}
