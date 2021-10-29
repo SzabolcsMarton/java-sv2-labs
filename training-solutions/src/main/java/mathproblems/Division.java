28b3ccb915be16835a1672b851d734780ac86f33
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-29 16:50:22 +0200

    add getRightNumbers(int[] number) method and tested in psvm

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -1,5 +1,8 @@
 package mathproblems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Division {
     public  void  getDivision( int number){
         for(int i = 1; i < number+1; i++){
@@ -9,8 +12,21 @@ public class Division {
         }
     }
 
+    public void getRightNumbers(int[] numbers){
+        for(int i = 1; i < numbers.length ; i++){
+            if(numbers[i] % i == 0 ){
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Division division = new Division();
-        division.getDivision(12);
+     //   division.getDivision(12);
+
+        int[] numbers = {2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10};
+
+        division.getRightNumbers(numbers);
     }
 }
