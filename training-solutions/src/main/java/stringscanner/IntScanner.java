400d927e8171dc90ac802b6cecca67c000f59e36
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-31 20:11:46 +0100

    add IntScanner with convertInt method and tested in main()

---
+++ training-solutions/src/main/java/stringscanner/IntScanner.java
@@ -0,0 +1,28 @@
+package stringscanner;
+
+import java.util.Scanner;
+
+public class IntScanner {
+    public String convertInts(String ints){
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = new Scanner(ints).useDelimiter(";");
+             while (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (number > 100) {
+                    sb.append(number);
+                    sb.append(", ");
+                }
+            }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        IntScanner scnr = new IntScanner();
+
+        System.out.println(scnr.convertInts("5;3;107;12;123;18;198"));
+        System.out.println(scnr.convertInts("107"));
+        System.out.println(scnr.convertInts("5;2;3;7"));
+        System.out.println(scnr.convertInts(""));
+    }
+}
