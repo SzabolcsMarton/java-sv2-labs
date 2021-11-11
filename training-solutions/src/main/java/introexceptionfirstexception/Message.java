112a4c8b9e9d73f3d3fbfe543d1a1fd2e5264fab
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-11 19:13:10 +0100

    decode the otherMessage

--- training-solutions/src/main/java/introexceptionfirstexception/Message.java
+++ training-solutions/src/main/java/introexceptionfirstexception/Message.java
@@ -9,7 +9,7 @@ public class Message {
                 "122", "233", "112", "32", "110", "97", "112", "111", "100", "33");
 
         List<String> anotherCodedMessage = Arrays.asList("69", "122", "32", "101", "103", "121", "32",
-                "104", "105", "98", "225", "115", "32", "252", "122", "1O1", "110", "101", "116", "46");
+                "104", "105", "98", "225", "115", "32", "252", "122", "101", "110", "101", "116", "46");
 
         for (int i = 0; i < codedMessage.size(); i++){
             int toInt = Integer.parseInt(anotherCodedMessage.get(i));
