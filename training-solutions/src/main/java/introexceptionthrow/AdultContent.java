761aa90ee7154df6ff7aacabee9e3c7b02370fb5
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-12 17:20:34 +0100

    add Adult, AdultContent and throw exception for age under 18

---
+++ training-solutions/src/main/java/introexceptionthrow/AdultContent.java
@@ -0,0 +1,11 @@
+package introexceptionthrow;
+
+public class AdultContent {
+    public static void main(String[] args) {
+        Adult adult = new Adult("Sanyi", 17);
+        //Adult adult = new Adult("Sanyi", 19);
+        System.out.println(adult.getName());
+    }
+
+
+}
