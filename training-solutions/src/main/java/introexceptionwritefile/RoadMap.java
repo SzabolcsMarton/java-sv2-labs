2e1482791ec040cf8c6c1bd998e6da1ad18e3f4d
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-15 17:29:43 +0100

    add a list. Instead of the root I wrote it to src/main/resources/roadmap.txt

--- training-solutions/src/main/java/introexceptionwritefile/RoadMap.java
+++ training-solutions/src/main/java/introexceptionwritefile/RoadMap.java
@@ -1,4 +1,23 @@
 package introexceptionwritefile;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class RoadMap {
+
+    public static void main(String[] args) {
+        List<String> roadMap = Arrays.asList("Budapest", "Szentendre", "Leányfalu", "Tahitótfalu", "Dunabogdány", "Visegrád", "Dömös");
+        Path path = Paths.get("src/main/resources/roadmap.txt");
+
+        try {
+            Files.write(path, roadMap);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Hiba a file irásakor!", ioe);
+        }
+    }
 }
