8fab080da99c07f01965a9563d86ee735d202d22
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-17 16:54:07 +0100

    add names and otherNames txt file. read the file and sorted out all 3 outcome option

--- training-solutions/src/main/java/introexceptionfinally/Names.java
+++ training-solutions/src/main/java/introexceptionfinally/Names.java
@@ -1,4 +1,34 @@
 package introexceptionfinally;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Names {
+
+    private String findNameWithPrefix(String prefix, List<String> allNames) {
+        for (String name : allNames) {
+            if (name.startsWith(prefix)) {
+                return name;
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Names names = new Names();
+        try{
+            //List<String> namesFromFile = Files.readAllLines(Paths.get("src/main/resources/names.txt"));
+           //List<String> namesFromFile = Files.readAllLines(Paths.get("src/main/resources/otherNames.txt"));
+             List<String> namesFromFile = Files.readAllLines(Paths.get("src/main/resources/names_.txt"));
+            System.out.println(names.findNameWithPrefix("dr", namesFromFile));
+        }catch (IOException ioe){
+            throw new IllegalStateException("Olvasasi hiba!", ioe);
+        }finally {
+            System.out.println("End of reading!");
+        }
+
+    }
 }
