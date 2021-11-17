5698f7b98b5bb6db33afefa9e2d9c38af16115e2
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-17 17:05:10 +0100

    add try/catch with finally

--- training-solutions/src/main/java/introexceptionfinally/Numbers.java
+++ training-solutions/src/main/java/introexceptionfinally/Numbers.java
@@ -1,4 +1,23 @@
 package introexceptionfinally;
 
+import java.util.Scanner;
+
 public class Numbers {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int number = 1;
+         while (number % 2 !=0){
+             System.out.println("Adjon meg egy szamot!");
+             try {
+                 int scnrNumber = Integer.parseInt(scnr.nextLine());
+                 number = scnrNumber;
+                 System.out.println(number);
+             }catch (NumberFormatException nfe){
+                 throw new IllegalStateException("Nem szamot adott meg!!", nfe);
+             }finally {
+                 System.out.println("Vege a kornek!");
+             }
+         }
+    }
 }
