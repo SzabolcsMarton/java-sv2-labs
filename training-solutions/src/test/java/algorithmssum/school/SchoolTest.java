4f48fb37372aa957735277922a300d737dd3d27d
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-18 08:46:23 +0100

    wrote junit5 test to test the sum.

--- training-solutions/src/test/java/algorithmssum/school/SchoolTest.java
+++ training-solutions/src/test/java/algorithmssum/school/SchoolTest.java
@@ -2,6 +2,9 @@ package algorithmssum.school;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolTest {
@@ -9,6 +12,12 @@ class SchoolTest {
     School school = new School();
 
     @Test
+    void getNumberOfStudentsTest(){
+        List<Integer> studentsOfClasses = Arrays.asList(20, 30, 40, 20, 35);
 
+        int numberOfPupils = school.getNumberOfStudents(studentsOfClasses);
+
+        assertEquals(145, numberOfPupils);
+    }
 
 }
