caf47acb7950d6527a8f64afc9d52f9cef994b1e
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-19 11:18:14 +0100

    add getMin method

--- training-solutions/src/main/java/algorithmsmax/temperature/Temperature.java
+++ training-solutions/src/main/java/algorithmsmax/temperature/Temperature.java
@@ -1,4 +1,17 @@
 package algorithmsmax.temperature;
 
+import java.util.List;
+
 public class Temperature {
+
+    public int getMin(List<Integer> degrees){
+        int min = Integer.MAX_VALUE;
+        for (int actual : degrees){
+            if(actual < min){
+                min = actual;
+            }
+        }
+        return min;
+    }
+
 }
