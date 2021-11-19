d1ae2118d16c94af58acadf801a836a880da3ef1
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-19 14:40:29 +0100

    add selectSalesPersonWithFurthestBelowTarget

--- training-solutions/src/main/java/algorithmsmax/sales/Sales.java
+++ training-solutions/src/main/java/algorithmsmax/sales/Sales.java
@@ -4,28 +4,43 @@ import java.util.List;
 
 public class Sales {
 
-    private int getDifferent(Salesperson salesperson){
-        return salesperson.getAmount() - salesperson.getTarget();
+    private int getDiffHeigher(Salesperson sp) {
+        return sp.getAmount() - sp.getTarget();
     }
 
-    public Salesperson selectSalesPersonWithMaxSalesAmount(List<Salesperson> sales){
+    private int getDiffLower(Salesperson sp) {
+        return sp.getTarget() - sp.getAmount();
+    }
+
+    public Salesperson selectSalesPersonWithMaxSalesAmount(List<Salesperson> sales) {
         Salesperson salesperson = null;
-        for (Salesperson actual : sales){
-            if(salesperson == null || actual.getAmount() > salesperson.getAmount()){
+        for (Salesperson actual : sales) {
+            if (salesperson == null || actual.getAmount() > salesperson.getAmount()) {
                 salesperson = actual;
             }
         }
         return salesperson;
     }
 
-    public Salesperson selectSalesPersonWithFurthestAboveTarget(List<Salesperson> sales){
+    public Salesperson selectSalesPersonWithFurthestAboveTarget(List<Salesperson> sales) {
         Salesperson salesperson = null;
-        for (Salesperson actual : sales){
-            if(salesperson == null || getDifferent(actual ) > getDifferent(salesperson)){
+        for (Salesperson actual : sales) {
+            if (salesperson == null || getDiffHeigher(actual) > getDiffHeigher(salesperson)) {
                 salesperson = actual;
             }
         }
         return salesperson;
+    }
+
+    public Salesperson selectSalesPersonWithFurthestBelowTarget(List<Salesperson> sales) {
+        Salesperson salesperson = null;
+        for (Salesperson actual : sales) {
+            if (salesperson == null || getDiffLower(actual) > getDiffLower(salesperson)) {
+                salesperson = actual;
+            }
+        }
+        return salesperson;
+
     }
 
 }
