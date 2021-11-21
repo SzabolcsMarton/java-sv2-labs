10e8f72684911f72ece4a74791098dbe82f2967a
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-21 14:30:13 +0100

    add Family class, with getAgesOfFamilyMembersWithNameGiven method

---
+++ training-solutions/src/main/java/algorithmstransformation/family/Family.java
@@ -0,0 +1,31 @@
+package algorithmstransformation.family;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Family {
+
+    private List<FamilyMember> familyMembers;
+
+    public Family() {
+        this.familyMembers = new ArrayList<>();
+    }
+
+    public List<Integer> getAgesOfFamilyMembersWithNameGiven(String name) {
+        List<Integer> result = new ArrayList<>();
+        for (FamilyMember actual : familyMembers) {
+            if (actual.getName().contains(name)) {
+                result.add(actual.getAge());
+            }
+        }
+        return result;
+    }
+
+    public List<FamilyMember> getFamilyMembers() {
+        return familyMembers;
+    }
+
+    public void addFamilyMember(FamilyMember newMember) {
+        familyMembers.add(newMember);
+    }
+}
