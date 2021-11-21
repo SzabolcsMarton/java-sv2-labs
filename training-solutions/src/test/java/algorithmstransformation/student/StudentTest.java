fec1d678d48437fb5754f77470da7e948a9924b3
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-21 17:56:52 +0100

    Student tested with junit5

---
+++ training-solutions/src/test/java/algorithmstransformation/student/StudentTest.java
@@ -0,0 +1,17 @@
+package algorithmstransformation.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void createStudentTest(){
+        Student student = new Student("Kovacs Pisti","Budapest Fo utca 114.");
+
+        assertEquals("Kovacs Pisti",student.getName());
+        assertEquals("Budapest Fo utca 114.", student.getAddress());
+    }
+
+}
