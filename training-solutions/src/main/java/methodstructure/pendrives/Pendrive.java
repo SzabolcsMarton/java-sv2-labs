aa7782b632719399f46750be6a17336b39ea8e7f
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-23 22:23:15 +0100

    add pendrives pckg. add Pendrive and Pendrives class.

---
+++ training-solutions/src/main/java/methodstructure/pendrives/Pendrive.java
@@ -0,0 +1,51 @@
+package methodstructure.pendrives;
+
+public class Pendrive {
+
+    private String name;
+    private int capacity;
+    private double price;
+
+    private double getPricePerCapacity(){
+        return price / capacity;
+    }
+
+    public void risePrise(int percent) {
+        price += price * (percent / 100.0);
+    }
+
+    public int comparePricePerCapacity(Pendrive other){
+        double pricePerCapacity = getPricePerCapacity();
+        double otherPricePerCapacity = other.getPricePerCapacity();
+        if(pricePerCapacity > otherPricePerCapacity){
+            return 1;
+        }
+        if(pricePerCapacity < otherPricePerCapacity){
+            return  -1;
+        }
+        return 0;
+    }
+
+    public boolean isCheaperThan(Pendrive other){
+        return this.price < other.getPrice();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pendrive { name='" + name +
+                ", capacity=" + capacity +
+                ", price=" + price + " }";
+    }
+}
