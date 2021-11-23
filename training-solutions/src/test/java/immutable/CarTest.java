e5dd2026d176af788bb473129efff8b00368dd92
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-23 09:06:29 +0100

    tested with null and empty brand and wrong year

--- training-solutions/src/test/java/immutable/CarTest.java
+++ training-solutions/src/test/java/immutable/CarTest.java
@@ -15,4 +15,26 @@ class CarTest {
        assertEquals(2010,car.getYearOfCreation());
     }
 
+    @Test
+    void createCarWithNullBrandTest(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                ()-> new Car(null,"Corsa", 1998));
+        assertEquals("Brand name cannot be empty!",exception.getMessage());
+    }
+
+    @Test
+    void createCarWithEmptyBrandTest(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                ()-> new Car("","Corsa", 1998));
+        assertEquals("Brand name cannot be empty!",exception.getMessage());
+    }
+
+    @Test
+    void createCarWithWrongYearTest(){
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                    ()-> new Car("Opel","Corsa", 2022));
+            assertEquals("Year of production cannot be in the future!",exception.getMessage());
+
+    }
+
 }
