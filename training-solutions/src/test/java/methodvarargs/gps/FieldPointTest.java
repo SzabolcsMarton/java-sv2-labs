98c9f6971f10ca074d56c68b4e513fa30bec2cc4
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-26 14:27:42 +0100

    Fieldpoint creation and add timeOfLogging tested

---
+++ training-solutions/src/test/java/methodvarargs/gps/FieldPointTest.java
@@ -0,0 +1,22 @@
+package methodvarargs.gps;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldPointTest {
+
+    @Test
+    void createFieldPointTest(){
+        LocalDateTime time = LocalDateTime.of(2020,8,5,15,20);
+        FieldPoint fieldPoint = new FieldPoint(time,10.2545,15.2565);
+        LocalDateTime logTime = LocalDateTime.of(2020,8,5,15,20);
+        fieldPoint.setTimeOfLogging(logTime);
+
+        assertEquals(logTime, fieldPoint.getTimeOfLogging());
+        assertEquals(10.2545, fieldPoint.getLatitude(), 0.00001);
+    }
+
+}
---
+++ training-solutions/src/test/java/methodvarargs/gps/HikingTourTest.java
@@ -0,0 +1,12 @@
+package methodvarargs.gps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HikingTourTest {
+
+    @Test
+    void getFieldPointsTest() {
+    }
+}
