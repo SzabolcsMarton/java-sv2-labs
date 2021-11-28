29df64949d863b1d4bd28054d71432bbe599b735
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-28 16:25:45 +0100

    add CountryCode enum, SimpleDateFormatter class. add formatDateString, formatDateStringByCountryCode methods and constructor with no params.

---
+++ training-solutions/src/main/java/defaultconstructor/date/SimpleDateFormatter.java
@@ -0,0 +1,26 @@
+package defaultconstructor.date;
+
+public class SimpleDateFormatter {
+
+    private CountryCode defaultCountryCode;
+
+    public SimpleDateFormatter(){
+        this.defaultCountryCode = CountryCode.HU;
+    }
+
+    public String formatDateString(SimpleDate simpleDate){
+        return formatDateStringByCountryCode(defaultCountryCode, simpleDate);
+
+    }
+
+    public String formatDateStringByCountryCode(CountryCode countryCode, SimpleDate simpleDate) {
+        if(countryCode == CountryCode.HU){
+            return simpleDate.getYear() + "-" + simpleDate.getMonth() + "-" + simpleDate.getDay();
+        }
+        if(countryCode == CountryCode.EN){
+            return simpleDate.getMonth() + "-" + simpleDate.getDay() + "-" + simpleDate.getYear();
+        }
+        return simpleDate.getDay() + "-" + simpleDate.getMonth() + "-" + simpleDate.getYear();
+    }
+
+}
