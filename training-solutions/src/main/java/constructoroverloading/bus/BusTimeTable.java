836f860603974d4b5f4936d9ed6c02e72b1e289f
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-28 23:14:10 +0100

    Add BusTimeTable with 2 constructor and getNext method

---
+++ training-solutions/src/main/java/constructoroverloading/bus/BusTimeTable.java
@@ -0,0 +1,34 @@
+package constructoroverloading.bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusTimeTable {
+
+    private List<SimpleTime> timeTable;
+
+    public BusTimeTable(List<SimpleTime> timeTable){
+        this.timeTable = timeTable;
+    }
+
+    public BusTimeTable(int firstHour, int lastHour, int everyMinute){
+        List<SimpleTime> result = new ArrayList<>();
+        for (int i = firstHour; i < lastHour; i++) {
+            result.add(new SimpleTime(i, everyMinute));
+        }
+        this.timeTable = result;
+    }
+
+    public List<SimpleTime> getTimeTable() {
+        return timeTable;
+    }
+
+    public SimpleTime getNextBus(SimpleTime actual){
+        for (int i = 0; i < timeTable.size(); i++){
+            if(timeTable.get(i).getDifference(actual) >= 0 ){
+                return timeTable.get(i);
+            }
+        }
+        throw new IllegalStateException("No more bus!");
+    }
+}
