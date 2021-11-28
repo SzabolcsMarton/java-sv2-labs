ad505c11eb4315e1a2607801e9bce7ed15bd66f6
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-28 09:23:19 +0100

    Vet addDog tested

--- training-solutions/src/test/java/staticattrmeth/vet/VetTest.java
+++ training-solutions/src/test/java/staticattrmeth/vet/VetTest.java
@@ -6,7 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VetTest {
 
+    @Test
+    void addDogTest(){
+        Vet vet = new Vet();
+        vet.addDog(new Dog("Bloki",6,Species.LABRADOR));
 
+        assertEquals(2,Vet.getCode());
+        assertEquals(1,vet.getRegisteredDogs().get(0).getCodeNr());
+    }
 
 
 }
