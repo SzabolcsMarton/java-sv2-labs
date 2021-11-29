e6a8f17f61887203c53db96b6e66f1ce5b30f895
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-29 22:57:34 +0100

    Room creation tested

---
+++ training-solutions/src/test/java/inheritanceconstructor/classroom/RoomTest.java
@@ -0,0 +1,17 @@
+package inheritanceconstructor.classroom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void createRoomTest(){
+        Room room = new Room("Foldszint", 30);
+
+        assertEquals(30, room.getCapacity());
+        assertEquals("Foldszint", room.getLocation());
+    }
+
+}
