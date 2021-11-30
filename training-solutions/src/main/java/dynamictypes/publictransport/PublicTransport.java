54557d7ae6fce2f0cc1c9e87d3e7620c5ebdcba3
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-30 11:15:19 +0100

    add PublicTransport class with a list of PublicVehicle

---
+++ training-solutions/src/main/java/dynamictypes/publictransport/PublicTransport.java
@@ -0,0 +1,17 @@
+package dynamictypes.publictransport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PublicTransport {
+
+    private List<PublickVehicle> vehiles = new ArrayList<>();
+
+    public void Vehicle(PublickVehicle vehicle){
+        vehiles.add(vehicle);
+    }
+
+    public List<PublickVehicle> getVehiles() {
+        return vehiles;
+    }
+}
