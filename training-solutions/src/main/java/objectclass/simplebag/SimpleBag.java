c0c6daa528634e811da40df0784eeacccd6beaf3
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-01 09:24:30 +0100

    finished SimpleBag and add Beer class

--- training-solutions/src/main/java/objectclass/simplebag/SimpleBag.java
+++ training-solutions/src/main/java/objectclass/simplebag/SimpleBag.java
@@ -1,5 +1,48 @@
 package objectclass.simplebag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleBag {
 
+    private List<Object> items = new ArrayList<>();
+
+    private int coursor;
+
+    public SimpleBag() {
+        beforeFirst();
+    }
+
+    public void putItem(Object item) {
+        items.add(item);
+    }
+
+    public boolean isEmpty() {
+        return items.size() == 0;
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void beforeFirst() {
+        coursor = -1;
+    }
+
+    public boolean hasNext() {
+        return !(isEmpty() || coursor >= items.size() - 1);
+    }
+
+    public Object next(){
+        coursor++;
+        return items.get(coursor);
+    }
+
+    public boolean contains(Object item) {
+        return items.contains(item);
+    }
+
+    public int getCursor(){
+        return coursor;
+    }
 }
