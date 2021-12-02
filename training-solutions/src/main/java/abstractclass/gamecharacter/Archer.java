d5b1cc42ad6c9672cfb4de8248529b36cdb2f69d
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-02 11:13:16 +0100

    add Archer class exten Character class overided the secondaryAttack abstract method

---
+++ training-solutions/src/main/java/abstractclass/gamecharacter/Archer.java
@@ -0,0 +1,31 @@
+package abstractclass.gamecharacter;
+
+import java.util.Random;
+
+public class Archer extends Character{
+
+    private int numberOfArrow;
+
+    public Archer(Point position, Random random) {
+        super(position, random);
+        this.numberOfArrow = 100;
+    }
+
+    private int getActualSecondaryDamage(){
+        return getRandom().nextInt(1,6);
+    }
+
+    private void shootingAnArrow(Character enemy){
+        numberOfArrow--;
+        hit(enemy, getActualSecondaryDamage());
+    }
+
+    @Override
+    public void secondaryAttack(Character enemy) {
+        shootingAnArrow(enemy);
+    }
+
+    public int getNumberOfArrow() {
+        return numberOfArrow;
+    }
+}
