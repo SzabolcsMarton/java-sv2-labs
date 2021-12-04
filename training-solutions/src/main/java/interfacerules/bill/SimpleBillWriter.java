f6f87f4e7d951d19d4fdebbd0b771da4986549a8
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-04 12:18:43 +0100

    add figure.AreaTest tested triangle, circle, and rectangle.

---
+++ training-solutions/src/main/java/interfacerules/bill/SimpleBillWriter.java
@@ -0,0 +1,11 @@
+package interfacerules.bill;
+
+import java.util.List;
+
+public class SimpleBillWriter implements BillWriter{
+
+    @Override
+    public String writeBill(List<String> billItems) {
+        return null;
+    }
+}
