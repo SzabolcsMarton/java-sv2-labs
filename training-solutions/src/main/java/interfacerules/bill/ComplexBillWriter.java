a1a11ceffb3ecd51d6196e03c783266735bb1891
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-04 12:35:06 +0100

    finished ComplexBillWriter implemented writeBill method

--- training-solutions/src/main/java/interfacerules/bill/ComplexBillWriter.java
+++ training-solutions/src/main/java/interfacerules/bill/ComplexBillWriter.java
@@ -2,10 +2,17 @@ package interfacerules.bill;
 
 import java.util.List;
 
-public class ComplexBillWriter implements BillWriter{
+public class ComplexBillWriter implements BillWriter {
 
     @Override
     public String writeBill(List<String> billItems) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (String actual : billItems) {
+            String[] temp = actual.split(";");
+            int pieces = Integer.parseInt(temp[2]);
+            int price = Integer.parseInt(temp[1]);
+            sb.append(String.format("%s; darabszám: %d, egységár: %d, összesen: %d Ft", temp[0], pieces, price, (pieces * price))).append("\n");
+        }
+        return sb.toString();
     }
 }
