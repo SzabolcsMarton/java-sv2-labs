28bffe94de94f2bdf0c1e7e5413f57a8b0e17fce
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-04 12:33:27 +0100

    finished SimpleBillWriter class

--- training-solutions/src/main/java/interfacerules/bill/SimpleBillWriter.java
+++ training-solutions/src/main/java/interfacerules/bill/SimpleBillWriter.java
@@ -3,9 +3,15 @@ package interfacerules.bill;
 import java.util.List;
 
 public class SimpleBillWriter implements BillWriter{
-
     @Override
     public String writeBill(List<String> billItems) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (String actual : billItems) {
+            String[] temp = actual.split(";");
+            int pieces = Integer.parseInt(temp[2]);
+            int price = Integer.parseInt(temp[1]);
+            sb.append(String.format("%s, %d * %d = %d Ft", temp[0], pieces, price, (pieces * price))).append("\n");
+        }
+        return sb.toString();
     }
 }
