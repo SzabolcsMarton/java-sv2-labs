171c7ad33834310925afd26df96a6e9c61913d49
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-04 11:37:02 +0100

    add ParkingPlace class and finished Equator class

--- training-solutions/src/main/java/interfacerules/commonproperties/Equator.java
+++ training-solutions/src/main/java/interfacerules/commonproperties/Equator.java
@@ -5,16 +5,13 @@ public class Equator implements Name, Length {
     private static final String NAME = "Equator";
     private static final double LENGTH = 40_075.0;
 
-    public Equator() {
-    }
-
     @Override
     public double getLength() {
-        return 0;
+        return LENGTH;
     }
 
     @Override
     public String getName() {
-        return null;
+        return NAME;
     }
 }
