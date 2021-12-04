c8b692440e74edd465e6963ad5834615867dba4b
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-04 11:51:59 +0100

    created bill pckg. add Bill class and created billitem.txt

---
+++ training-solutions/src/main/java/interfacerules/bill/Bill.java
@@ -0,0 +1,22 @@
+package interfacerules.bill;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Bill {
+
+    public List<String> readBillItemsFromFile(Path path){
+        try {
+            List<String> result = Files.readAllLines(path);
+            return result;
+        }catch (IOException ioe){
+            throw new IllegalArgumentException("Can not read file", ioe);
+        }
+    }
+
+
+
+}
