972edf5a8f9d5a0053627b2486e184639213cf8d
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-17 16:51:29 +0100

    add CanCarryPassengerBehavior class

---
+++ training-solutions/src/test/java/isahasa/fleet/CanCarryPassengerBehavior.java
@@ -0,0 +1,29 @@
+package isahasa.fleet;
+
+public class CanCarryPassengerBehavior implements CanCarryPassengers{
+
+    private int passengers;
+
+    private int maxPassengers;
+
+    public CanCarryPassengerBehavior(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+
+    @Override
+    public int loadPassengers(int passengers) {
+        int leftOver = 0;
+        if(passengers <= maxPassengers){
+            this.passengers = passengers;
+        }else{
+            leftOver = passengers - maxPassengers;
+            this.passengers = maxPassengers;
+        }
+        return leftOver;
+    }
+
+    @Override
+    public int getPassengers() {
+        return passengers;
+    }
+}
