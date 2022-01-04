3d3f9a06b6bb0cd078cdf4eede23acc84be06295
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-04 12:45:15 +0100

    created measurement pckg. add FileReader, and validators

---
+++ training-solutions/src/main/java/exceptions/measurement/Measurement.java
@@ -0,0 +1,63 @@
+package exceptions.measurement;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Measurement {
+
+    public List<String> fileReader(Path path) {
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file!");
+        }
+    }
+
+    public List<String> validate(List<String> measurment) {
+        List<String> faults = new ArrayList<>();
+        for (String line : measurment) {
+            if (!validateWholeLine(line)) {
+                faults.add(line);
+            }
+        }
+        return faults;
+    }
+
+    private boolean validateWholeLine(String line) {
+        String[] lineParts = line.split(",");
+        return validateLineParts(lineParts) &&
+                validateStringToNumber(lineParts[0]) &&
+                validateMeasuredValue(lineParts[1]) &&
+                validateName(lineParts[2]);
+    }
+
+    private boolean validateLineParts(String[] line) {
+        return line.length == 3;
+    }
+
+    private boolean validateStringToNumber(String numberString) {
+        try {
+            int number = Integer.parseInt(numberString);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    private boolean validateMeasuredValue(String numberString) {
+        try {
+            double measuredValue = Double.parseDouble(numberString);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    private boolean validateName(String nameString) {
+        String[] nameParts = nameString.split(" ");
+        return nameParts.length == 2;
+    }
+}
