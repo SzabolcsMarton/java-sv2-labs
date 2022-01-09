383730719a8852b4762165cb968112a7ba0f1df5
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-09 13:22:03 +0100

    add siblingnames.txt in testresources, wrote test SiblingsTestand run succesfully

---
+++ training-solutions/src/test/java/exceptionresource/siblings/SiblingsTest.java
@@ -0,0 +1,31 @@
+package exceptionresource.siblings;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SiblingsTest {
+
+    Siblings siblings = new Siblings();
+
+    @Test
+    void getFullNameTest(){
+        List<String> testResult = siblings.getFullNames("Kovacs", Paths.get("src/test/resources/siblingsnames.txt"));
+
+        assertEquals(3,testResult.size());
+        assertEquals("Kovacs Anita",testResult.get(1));
+    }
+
+    @Test
+    void getFullNameTestWithWrongPath(){
+        Path path = Paths.get("src/test/resources/nonames.txt");
+        Exception exception = assertThrows(IllegalStateException.class,
+                ()-> siblings.getFullNames("Kovacs", path));
+        assertEquals("Can not read the file",exception.getMessage());
+    }
+
+}
