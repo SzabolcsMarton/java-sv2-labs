d69d970bed112611484ef28e92178bfcd2764111
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-09 13:49:18 +0100

    created hiddenword.HiddenWord class

---
+++ training-solutions/src/main/java/exceptionresource/hiddenword/HiddenWord.java
@@ -0,0 +1,36 @@
+package exceptionresource.hiddenword;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class HiddenWord {
+
+    public String getHiddenWord(Path path) {
+        try (Scanner scanner = new Scanner(path)) {
+            return getWordFromFile(scanner);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    private char findLetterCharacterInString(String stringToFindLetter) {
+        char[] characters = stringToFindLetter.toCharArray();
+        for (char actual : characters) {
+            if (!Character.isDigit(actual)) {
+                return actual;
+            }
+        }
+        return ' ';
+    }
+
+    private String getWordFromFile(Scanner scanner) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            char letterFromFile = findLetterCharacterInString(line);
+            stringBuilder.append(letterFromFile);
+        }
+        return stringBuilder.toString();
+    }
+}
