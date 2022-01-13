d22048e0d00b9b95a0f7bf3597425c2c697cafa3
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-13 17:44:14 +0100

    Add Company class, override equals and hashCode

---
+++ training-solutions/src/main/java/collectionsequalshash/Company.java
@@ -0,0 +1,35 @@
+package collectionsequalshash;
+
+import java.util.Objects;
+
+public class Company {
+
+    private String nameOfCompany;
+    private String taxNumber;
+
+    public Company(String nameOfCompany, String taxNumber) {
+        this.nameOfCompany = nameOfCompany;
+        this.taxNumber = taxNumber;
+    }
+
+    public String getNameOfCompany() {
+        return nameOfCompany;
+    }
+
+    public String getTaxNumber() {
+        return taxNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return nameOfCompany.equals(company.nameOfCompany) && taxNumber.equals(company.taxNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameOfCompany, taxNumber);
+    }
+}
