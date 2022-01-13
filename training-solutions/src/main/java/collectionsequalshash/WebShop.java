d8ad209dc91890c040d6ab015c595671cefac487
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-13 18:34:52 +0100

    Created webShop class tested findHowMany in main

--- training-solutions/src/main/java/collectionsequalshash/WebShop.java
+++ training-solutions/src/main/java/collectionsequalshash/WebShop.java
@@ -1,5 +1,6 @@
 package collectionsequalshash;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WebShop {
@@ -18,6 +19,20 @@ public class WebShop {
             }
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        List<Product> productsInWebShop = new ArrayList<>();
+        productsInWebShop.add(new Product("aaa","111"));
+        productsInWebShop.add(new Product("bbb","222"));
+        productsInWebShop.add(new Product("ccc","111"));
+        productsInWebShop.add(new Product("ddd","333"));
+        productsInWebShop.add(new Product("eee","111"));
+        productsInWebShop.add(new Product("fff","444"));
+
+        WebShop webShop = new WebShop(productsInWebShop);
+
+        System.out.println(webShop.findHowMany(new Product("test","111")));
     }
 
 }
