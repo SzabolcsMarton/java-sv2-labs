29881861d9b9c2e6d4b63bfc96c695f1a3abb288
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-14 15:21:50 +0100

    add PhysicalEducation class tested in psvm

--- training-solutions/src/main/java/collectionsset/PhysicalEducation.java
+++ training-solutions/src/main/java/collectionsset/PhysicalEducation.java
@@ -1,5 +1,6 @@
 package collectionsset;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -14,4 +15,17 @@ public class PhysicalEducation {
         return result;
     }
 
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("a", 103));
+        students.add(new Student("b", 106));
+        students.add(new Student("c", 101));
+        students.add(new Student("d", 107));
+        students.add(new Student("e", 105));
+        students.add(new Student("f", 102));
+        students.add(new Student("g", 104));
+
+        PhysicalEducation pe = new PhysicalEducation();
+        System.out.println(pe.getOrderInLessons(students));
+    }
 }
