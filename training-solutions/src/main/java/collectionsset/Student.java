784b78874df73e3ecbb7b0e4c8628054f8f8d1bb
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-14 15:10:51 +0100

    Add Student class

---
+++ training-solutions/src/main/java/collectionsset/Student.java
@@ -0,0 +1,20 @@
+package collectionsset;
+
+public class Student {
+
+    private final String name;
+    private int hight;
+
+    public Student(String name, int hight) {
+        this.name = name;
+        this.hight = hight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHight() {
+        return hight;
+    }
+}
