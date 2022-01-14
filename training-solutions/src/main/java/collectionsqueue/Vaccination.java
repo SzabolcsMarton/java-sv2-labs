eb03d49ef9bfa72ff745d1ea8353d65656bfcc1c
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-14 17:20:53 +0100

    Vaccination tested

--- training-solutions/src/main/java/collectionsqueue/Vaccination.java
+++ training-solutions/src/main/java/collectionsqueue/Vaccination.java
@@ -1,5 +1,6 @@
 package collectionsqueue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -9,10 +10,26 @@ public class Vaccination {
     private Queue<Integer> getVaccinationOrder(List<Person> people){
         Queue<Integer> vaccinationOrder = new PriorityQueue<>();
         for (Person actual : people) {
-            if(actual.getAge() >= 18 || actual.getAge() <= 65) {
+            if(actual.getAge() >= 18 && actual.getAge() <= 65) {
                 vaccinationOrder.add(actual.getAge());
             }
         }
         return vaccinationOrder;
+    }
+
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("a",10));
+        people.add(new Person("b",35));
+        people.add(new Person("c",65));
+        people.add(new Person("d",30));
+        people.add(new Person("e",17));
+        people.add(new Person("f",19));
+        people.add(new Person("g",67));
+        Vaccination vaccination = new Vaccination();
+        PriorityQueue<Integer> result = (PriorityQueue<Integer>) vaccination.getVaccinationOrder(people);
+        System.out.println(result);
+        System.out.println(result.poll());
+        System.out.println(result.poll());
     }
 }
