2f0d284ba5fea99c23e4b09851afc13d1a9512fb
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-16 11:48:44 +0100

    created collectionsiterator pckg. add ChristmasPresent class

---
+++ training-solutions/src/main/java/collectionsiterator/ChristmasPresent.java
@@ -0,0 +1,20 @@
+package collectionsiterator;
+
+public class ChristmasPresent {
+
+    private String description;
+    private String name;
+
+    public ChristmasPresent(String description, String name) {
+        this.description = description;
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
