ad23091a2c314c1d230739fb27ac32338090f013
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-17 00:29:50 +0100

    issuetracker deleted must to start from begining, week12.md updated

--- training-solutions/src/main/java/clone/issuetracker/CopyMode.java
+++
@@ -1,5 +0,0 @@
-package clone.issuetracker;
-
-public enum CopyMode {
-    WITH_COMMENTS, WITHOUT_COMMENTS
-}
--- training-solutions/src/main/java/clone/issuetracker/Status.java
+++
@@ -1,5 +0,0 @@
-package clone.issuetracker;
-
-public enum Status {
-    NEW, IN_PROGRESS, CLOSED
-}
