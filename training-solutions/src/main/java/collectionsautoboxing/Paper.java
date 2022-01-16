a7451b7c9acbf1781d8bfd9fbd44fe78d08b3f42
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-16 10:09:52 +0100

    Add paper class

---
+++ training-solutions/src/main/java/collectionsautoboxing/Paper.java
@@ -0,0 +1,47 @@
+package collectionsautoboxing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Paper {
+
+    private Map<String, Integer> classPerKg = new HashMap<>();
+
+    public void putFurtherPaper(String className, int kilogramms){
+       if( kilogramms == 0)
+           return;
+       if(!classPerKg.containsKey(className)){
+           classPerKg.put(className,kilogramms);
+       }else {
+           classPerKg.put(className, classPerKg.get(className) + kilogramms);
+       }
+    }
+
+    public String getWinnerClass(){
+        int highestKg = Integer.MIN_VALUE;
+        String resultClass = "";
+
+        for (String actualclass : classPerKg.keySet()){
+            if(classPerKg.get(actualclass) > highestKg){
+                resultClass = actualclass;
+            }
+        }
+
+        return resultClass;
+    }
+
+    public int getTotalWeight(){
+        int result = 0;
+        for (Integer actual : classPerKg.values()){
+            result += actual;
+        }
+
+        return result;
+    }
+
+
+
+    public Map<String, Integer> getClassPerKg() {
+        return classPerKg;
+    }
+}
