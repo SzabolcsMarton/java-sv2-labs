11092fa0d8621cebdc0b98850a871a01f65cd10a
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-16 17:52:28 +0100

    created searching pckg. add Pike class

---
+++ training-solutions/src/main/java/searching/Pike.java
@@ -0,0 +1,20 @@
+package searching;
+
+public class Pike {
+
+    private final String pikeName;
+    private final int height;
+
+    public Pike(String pikeName, int height) {
+        this.pikeName = pikeName;
+        this.height = height;
+    }
+
+    public String getPikeName() {
+        return pikeName;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
