8b0daccd409e2fd97070c40451abe5d984aedf69
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-18 19:23:20 +0100

    add BucketList class

---
+++ training-solutions/src/main/java/optional/BucketList.java
@@ -0,0 +1,36 @@
+package optional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BucketList {
+
+    private List<Destination> destinations = new ArrayList<>();
+
+    public Optional<Destination> getDestinationWithKeyword(String keyword){
+        if(destinations.isEmpty()){
+            return Optional.empty();
+        }
+        for(Destination actual : destinations){
+            if(actual.getName().contains(keyword)){
+                return Optional.of(actual);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public Optional<Destination> getDestinationNearerThanGiven(int maxKm) {
+        if(destinations.isEmpty()){
+            return Optional.empty();
+        }
+        for (Destination actual : destinations){
+            if(actual.getKmFromHome() < maxKm) {
+                return Optional.of(actual);
+            }
+        }
+
+        return Optional.empty();
+    }
+}
