9574fa3ffb5821ba6953ad26ab3a1b8a3b50fa35
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-19 10:34:35 +0100

    add Read class

---
+++ training-solutions/src/main/java/streams/Read.java
@@ -0,0 +1,39 @@
+package streams;
+
+import java.text.Collator;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Read {
+
+    private List<Book> books = new ArrayList<>();
+
+    public List<String> listBookTitlesShorterThanGiven(int maxNumberOfPages){
+        return  books.stream()
+                .filter((book -> book.getNumberOfPages() < maxNumberOfPages))
+                .map(Book::getTitle)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> listBookTitlesWithGivenAuthor(String author) {
+        return books.stream()
+                .map(Book::getAuthor)
+                .filter(bookAuthor -> bookAuthor.equals(author))
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+    }
+
+    public List<String> listAuthors(){
+        return books.stream()
+                .map(Book::getAuthor)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+    public List<Book> getBooks() {
+        return books;
+    }
+}
