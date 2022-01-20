671fbe250c54093325de6ec0e35af24d1043c418
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-20 09:12:01 +0100

    lambdacomparator.cloud.CloudStorage implemented constructors and implemented Comparable interface

--- training-solutions/src/main/java/lambdacomparator/cloud/CloudStorage.java
+++ training-solutions/src/main/java/lambdacomparator/cloud/CloudStorage.java
@@ -1,14 +1,20 @@
 package lambdacomparator.cloud;
 
-public class CloudStorage {
+public class CloudStorage implements Comparable<CloudStorage>{
 
     private String provider;
     private int space;
     private double price;
+    private PayPeriod payPeriod;
 
-    public CloudStorage(String provider, int space, double price) {
+    public CloudStorage(String provider, int space) {
         this.provider = provider;
         this.space = space;
+    }
+
+    public CloudStorage(String provider, int space, PayPeriod payPeriod, double price){
+        this(provider,space);
+        this.payPeriod = payPeriod;
         this.price = price;
     }
 
@@ -22,5 +28,23 @@ public class CloudStorage {
 
     public double getPrice() {
         return price;
+    }
+
+    @Override
+    public int compareTo(CloudStorage other) {
+        double value = 0;
+        double otherValue = 0;
+        if(this.payPeriod == null){
+            return 0;
+        }else {
+            value = (this.price * 12 / this.payPeriod.getLength())/ this.space * 1000;
+        }
+        if(other.payPeriod == null){
+            return 0;
+        }else {
+            otherValue = (other.price * 12 / other.payPeriod.getLength())/ other.space * 1000;
+        }
+
+        return Double.compare(value, otherValue);
     }
 }
