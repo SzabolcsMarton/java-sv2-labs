5ab19e1d8fa40c8270aa2a7757ff68ca07dce48b
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-21 18:24:25 +0100

    created lambdaintermediate pckg , add LongWords class implemented the three methods

---
+++ training-solutions/src/main/java/lambdaintermediate/LongWords.java
@@ -0,0 +1,41 @@
+package lambdaintermediate;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class LongWords {
+
+    private List<String> words;
+
+    public LongWords(List<String> words) {
+        this.words = words;
+    }
+
+    public long getNumberOfWordsStartWith(String keyWord) {
+        return words.stream()
+                .filter(word -> word.startsWith(keyWord))
+                .count();
+    }
+
+    public List<String> getLongerWords(int number) {
+        return words
+                .stream()
+                .filter(word -> word.length() > number)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<String> getShortestWordContainingGivenCharacter(char character) {
+        return words
+                .stream()
+                .filter(s -> s.indexOf(character) > 0)
+                .min(Comparator.comparing(String::length));
+    }
+
+
+    public List<String> getWords() {
+        return new ArrayList<>(words);
+    }
+}
