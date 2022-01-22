8c8195207017929550789640026407f0338db166
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-22 13:55:07 +0100

    add Truck class implement the methods for search and statistic

---
+++ training-solutions/src/main/java/lambdaprimitive/Truck.java
@@ -0,0 +1,62 @@
+package lambdaprimitive;
+
+import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+public class Truck {
+
+    private List<Cargo> thingsToLoad;
+
+    public Truck(List<Cargo> thingsToLoad) {
+        this.thingsToLoad = thingsToLoad;
+    }
+
+    public double getShortestLength() {
+        return thingsToLoad
+                .stream()
+                .mapToDouble(Cargo::getLength)
+                .min().orElse(0.0);
+    }
+
+    public int getTotalWeight() {
+        return thingsToLoad
+                .stream()
+                .mapToInt(Cargo::getWeight)
+                .sum();
+    }
+
+    public double getAverageWeight() {
+        return thingsToLoad
+                .stream()
+                .mapToDouble(Cargo::getWeight)
+                .average()
+                .orElseThrow(() -> new IllegalArgumentException("No cargo."));
+    }
+
+    public int getMaxWeight() {
+        return thingsToLoad
+                .stream()
+                .mapToInt(Cargo::getWeight)
+                .max()
+                .orElse(0);
+    }
+
+    public String getWeightStatistics() {
+        IntSummaryStatistics stat = thingsToLoad
+                .stream()
+                .mapToInt(Cargo::getWeight)
+                .summaryStatistics();
+        if(stat.getCount() == 0) {
+            return "No cargo.";
+        }
+        return String.format("A rakományban található %d tétel, melyeknek összsúlya %d kg," +
+                " közülük a legnehezebb %d kg, a legkönnyebb %d kg. A súlyuk átlagosan %.1f kg.",
+                stat.getCount(),stat.getSum(),stat.getMax(),stat.getMin(),stat.getAverage());
+    }
+
+
+    public List<Cargo> getThingsToLoad() {
+        return new ArrayList<>(thingsToLoad);
+    }
+}
