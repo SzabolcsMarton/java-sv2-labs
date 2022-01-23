432a2dec5ca7b32484753d9391abc14ece89fe73
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-23 09:07:59 +0100

    add Honey class

---
+++ training-solutions/src/main/java/lambdacollectors/honey/Honey.java
@@ -0,0 +1,19 @@
+package lambdacollectors.honey;
+
+public class Honey {
+    private HoneyType type;
+    private GlassSize size;
+
+    public Honey(HoneyType type, GlassSize size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    public HoneyType getType() {
+        return type;
+    }
+
+    public GlassSize getSize() {
+        return size;
+    }
+}
