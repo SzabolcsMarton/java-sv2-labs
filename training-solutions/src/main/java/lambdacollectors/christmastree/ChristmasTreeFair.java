7992c2b8b2929c7b0101cd1b060c7baf948e1ad6
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-23 07:55:31 +0100

    finished ChristmasTreeFair class

--- training-solutions/src/main/java/lambdacollectors/christmastree/ChristmasTreeFair.java
+++ training-solutions/src/main/java/lambdacollectors/christmastree/ChristmasTreeFair.java
@@ -1,7 +1,7 @@
 package lambdacollectors.christmastree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChristmasTreeFair {
 
@@ -10,6 +10,35 @@ public class ChristmasTreeFair {
     public ChristmasTreeFair(List<ChristmasTree> xmasTrees) {
         this.xmasTrees = xmasTrees;
     }
+
+    public Map<PineTreeType, Long> getCountByType() {
+        return xmasTrees
+                .stream()
+                .collect(Collectors.groupingBy(ChristmasTree::getType,
+                        Collectors.counting()));
+    }
+
+    public double getMaxHeightByType(PineTreeType type) {
+        Optional<ChristmasTree> xmassTree =xmasTrees
+                .stream()
+                .filter(christmasTree -> christmasTree.getType().equals(type))
+                .collect(Collectors.maxBy(Comparator.comparing(ChristmasTree::getHeight)));
+        if(xmassTree.isEmpty()){
+            return 0.0;
+        }
+
+        return xmassTree.get().getHeight();
+    }
+
+    public double getAveragePrice() {
+        return xmasTrees
+                .stream()
+                .collect(Collectors.averagingDouble(ChristmasTree -> ChristmasTree.getType().getPricePerMeter()));
+    }
+
+
+//    : visszaadja az összes kapható fa árából számolt átlagos árat.
+//    Ha üres a lista, 0.0-t adjon vissza!
 
     public List<ChristmasTree> getXmasTrees() {
         return new ArrayList<>(xmasTrees);
