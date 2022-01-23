1c1c260f914fa1c2b8aec394dd5465c8d925c6e3
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-23 09:33:26 +0100

    add three collector methods

--- training-solutions/src/main/java/lambdacollectors/honey/BeeKeeper.java
+++ training-solutions/src/main/java/lambdacollectors/honey/BeeKeeper.java
@@ -2,6 +2,8 @@ package lambdacollectors.honey;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BeeKeeper {
 
@@ -10,6 +12,28 @@ public class BeeKeeper {
     public BeeKeeper(List<Honey> honeys) {
         this.honeys = honeys;
     }
+
+
+    public int getTotalValueOfGivenGlasses(GlassSize size) {
+        return honeys
+                .stream()
+                .filter(honey -> honey.getSize() == size)
+                .collect(Collectors.summingInt(honey -> (int) (honey.getType().getPricePerKg() * size.getKgPerGlass())));
+    }
+
+    public Map<GlassSize, Long> getAmountsOfGivenType(HoneyType type) {
+        return honeys
+                .stream()
+                .filter(honey -> honey.getType().equals(type))
+                .collect(Collectors.groupingBy(Honey::getSize,Collectors.counting()));
+    }
+
+    public Map<Boolean, List<Honey>> getGroupsByGivenTypeAndSize(HoneyType type, GlassSize size){
+        return honeys
+                .stream()
+                .collect(Collectors.partitioningBy(honey -> honey.getType().equals(type) && honey.getSize().equals(size)));
+    }
+
 
     public List<Honey> getHoneys() {
         return new ArrayList<>(honeys);
