b158f8aa1384c573304432cad63fe13ba7f1bdff
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-23 06:41:03 +0100

    finished PineTreeType enum,

--- training-solutions/src/main/java/lambdacollectors/christmastree/PineTreeType.java
+++ training-solutions/src/main/java/lambdacollectors/christmastree/PineTreeType.java
@@ -1,5 +1,15 @@
 package lambdacollectors.christmastree;
 
 public enum PineTreeType {
-    NORDMANN, SILVER, SPRUCE
+    NORDMANN(10_000), SILVER(8_000), SPRUCE(4_000);
+
+    private int pricePerMeter;
+
+    public int getPricePerMeter() {
+        return pricePerMeter;
+    }
+
+    PineTreeType(int pricePerMeter) {
+        this.pricePerMeter = pricePerMeter;
+    }
 }
