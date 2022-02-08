47cdff02e0c91388a41d1da5044d782e866de39d
SzabolcsMarton <maszabolcs@gmail.com>
2022-02-08 17:30:45 +0100

    add ActivityWithTrack class

---
+++ training-solutions/src/main/java/activity/ActivityWithTrack.java
@@ -0,0 +1,30 @@
+package activity;
+
+public class ActivityWithTrack implements Activity {
+
+    private Track track;
+    private ActivityType activityType;
+
+    public ActivityWithTrack(Track track, ActivityType activityType) {
+        this.track = track;
+        this.activityType = activityType;
+    }
+
+    @Override
+    public double getDistance() {
+        return track.getDistance();
+    }
+
+    @Override
+    public ActivityType getType() {
+        return activityType;
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public ActivityType getActivityType() {
+        return activityType;
+    }
+}
