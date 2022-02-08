99798abbc9db972c17eda5a2b5d1805f299348f4
SzabolcsMarton <maszabolcs@gmail.com>
2022-02-08 20:19:50 +0100

    add ActivityWithoutTrack class

--- training-solutions/src/test/java/activity/ActivityWithoutTrackTest.java
+++ training-solutions/src/test/java/activity/ActivityWithoutTrackTest.java
@@ -6,18 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ActivityWithoutTrackTest {
-//
-//    ActivityWithoutTrack activityWithoutTrack = new ActivityWithoutTrack(ActivityType.BASKETBALL);
-//
-//    @Test
-//    public void testGetDistance() {
-//        assertEquals(0.0, activityWithoutTrack.getDistance());
-//    }
-//
-//    @Test
-//    public void testGetType() {
-//        assertEquals(ActivityType.BASKETBALL, activityWithoutTrack.getType());
-//    }
+
+    ActivityWithoutTrack activityWithoutTrack = new ActivityWithoutTrack(ActivityType.BASKETBALL);
+
+    @Test
+    public void testGetDistance() {
+        assertEquals(0.0, activityWithoutTrack.getDistance());
+    }
+
+    @Test
+    public void testGetType() {
+        assertEquals(ActivityType.BASKETBALL, activityWithoutTrack.getType());
+    }
 
 
 }
