dc9332578f94dad84421b0f837d34c236ee8cb52
SzabolcsMarton <maszabolcs@gmail.com>
2022-02-08 20:31:15 +0100

    add Report class

--- training-solutions/src/test/java/activity/ActivitiesTest.java
+++ training-solutions/src/test/java/activity/ActivitiesTest.java
@@ -10,26 +10,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ActivitiesTest {
-//
-//    Track track = new Track();
-//
-//    Activities activities = new Activities(Arrays.asList(
-//            new ActivityWithTrack(track, ActivityType.RUNNING),
-//            new ActivityWithoutTrack(ActivityType.BASKETBALL),
-//            new ActivityWithTrack(track, ActivityType.RUNNING)));
-//
-//    @Test
-//    public void testNumberOfWithTrack() {
-//
-//        assertEquals(2, activities.numberOfTrackActivities());
-//    }
-//
-//    @Test
-//    public void testNumberOfWithoutTrack() {
-//
-//        assertEquals(1, activities.numberOfWithoutTrackActivities());
-//    }
-//
+
+    Track track = new Track();
+
+    Activities activities = new Activities(Arrays.asList(
+            new ActivityWithTrack(track, ActivityType.RUNNING),
+            new ActivityWithoutTrack(ActivityType.BASKETBALL),
+            new ActivityWithTrack(track, ActivityType.RUNNING)));
+
+    @Test
+    public void testNumberOfWithTrack() {
+
+        assertEquals(2, activities.numberOfTrackActivities());
+    }
+
+    @Test
+    public void testNumberOfWithoutTrack() {
+
+        assertEquals(1, activities.numberOfWithoutTrackActivities());
+    }
+
 //    @Test
 //    public void testGetReportType() {
 //        track.addTrackPoint(new TrackPoint(new Coordinate(12.5, 34.89), 123));
