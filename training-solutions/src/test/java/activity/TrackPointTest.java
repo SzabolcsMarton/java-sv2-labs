c14b15b6d909ed597af3244e0f91685cb18ad50c
SzabolcsMarton <maszabolcs@gmail.com>
2022-02-08 15:31:21 +0100

    add CalculateDistance helper class

--- training-solutions/src/test/java/activity/TrackPointTest.java
+++ training-solutions/src/test/java/activity/TrackPointTest.java
@@ -7,21 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TrackPointTest {
 
-//
-//    TrackPoint trackPoint = new TrackPoint(new Coordinate(12.5, 34.89), 123);
-//    TrackPoint trackPoint2 = new TrackPoint(new Coordinate(12.6, 34.123), 200);
-//
-//    @Test
-//    public void testCreateTrackPoint() {
-//
-//        assertEquals(34.89, trackPoint.getCoordinate().getLongitude());
-//        assertEquals(12.5, trackPoint.getCoordinate().getLatitude());
-//        assertEquals(123.0, trackPoint.getElevation());
-//
-//    }
-//
-//    @Test
-//    public void testGetDistance() {
-//        assertTrue(trackPoint.getDistanceFrom(trackPoint2) > 83988.01 && trackPoint.getDistanceFrom(trackPoint2) < 83988.11);
-//    }
+
+    TrackPoint trackPoint = new TrackPoint(new Coordinate(12.5, 34.89), 123);
+    TrackPoint trackPoint2 = new TrackPoint(new Coordinate(12.6, 34.123), 200);
+
+    @Test
+    public void testCreateTrackPoint() {
+
+        assertEquals(34.89, trackPoint.getCoordinate().getLongitude());
+        assertEquals(12.5, trackPoint.getCoordinate().getLatitude());
+        assertEquals(123.0, trackPoint.getElevation());
+
+    }
+
+    @Test
+    public void testGetDistance() {
+        assertTrue(trackPoint.getDistanceFrom(trackPoint2) > 83988.01 && trackPoint.getDistanceFrom(trackPoint2) < 83988.11);
+    }
 }
